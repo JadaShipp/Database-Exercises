cb159b35cfd2e63a974815c35c7f2e0f36251fc7
jadashipp <jada.l.shipp@gmail.com>
2020-02-07 15:01:25 -0600

    Removed 3.3_tables_exercises.sql

--- 3.3_tables_exercises.sql
+++
@@ -1,8 +0,0 @@
-# All tables in the database: departments, dept_emp, dept_manager, employees, employees_with_departments, salaries, titles
-# Data types of employees table include: INT, DATE, VARCHAR, ENUM
-#Tables containing numeric type column: Any table containing an employee number, date or salary (dept_emp,dept_manager, employees, employees_with_departments, salaries, titles)
-#Tables conaining string type: any table that includes an employee name, employee titles, or department name (dept_emp, dept_manager, employees, employees_with_departments, salaries)
-#Tables containing the date: Any table containing a component of time (titles, salaries, dept_emp,dept_manager, employees,)
-#Employees exist within departments 
-SHOW CREATE TABLE employees;
-SHOW CREATE TABLE dept_manager;
