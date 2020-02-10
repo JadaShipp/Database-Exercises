275e6415d78c4b612218d9805a6797cf49a252f5
jadashipp <jada.l.shipp@gmail.com>
2020-02-10 11:49:48 -0600

    Reupload Where Clause Practice with updates

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -4,12 +4,17 @@ SELECT * FROM employees;
 #all employees with first names 'Irena', 'Vidya', or 'Maya'
 SELECT *
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya','Maya');
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M'
 
 #all employees whose last name starts with 'E'
 SELECT *
 FROM employees
-WHERE last_name LIKE 'e%';
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+#all employees whose last name starts and ends with 'E'
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 
 #all employees hired in the 90s
 SELECT *
@@ -20,8 +25,18 @@ WHERE hire_date LIKE '199%';
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%12%' AND birth_date LIKE '%25%';
+#all employees hired in the 90s and born on Christmas
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%12%' AND birth_date LIKE '%25%';
+
 
 #all employees with a 'q' in their last name 
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
+
+#aLL employees WITH a 'q' IN their LAST NAME but NOT 'qu'
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
