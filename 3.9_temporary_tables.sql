1f4cdda1b1a82cf42763cac3ff5e401d4713c710
jadashipp <jada.l.shipp@gmail.com>
2020-02-13 15:27:35 -0600

    Update temporary tables

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -5,3 +5,10 @@ JOIN employees.dept_emp USING(emp_no)
 JOIN employees.departments USING(dept_no)
 LIMIT 100;
 
+ALTER TABLE employees_with_departments ADD full_name VARCHAR (30) NOT NULL;
+
+UPDATE employees_with_departments 
+SET full_name = CONCAT (first_name, " ", last_name);
+
+
+ 
