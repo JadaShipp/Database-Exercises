daa776aac2ceb248270547d46849cdb697de6753
jadashipp <jada.l.shipp@gmail.com>
2020-02-13 15:15:57 -0600

    Temporary Table Exercise

---
+++ 3.9_temporary_tables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees_with_departments AS
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+LIMIT 100;
+
